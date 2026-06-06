9a812c665c6f062eed4fb5805b2b30e42a409a9b
hardtanh <fcdjin@hardtanh.com>
2026-06-06 13:43:04 -0500

    initial commit

--- deploy2/main.py
+++ deploy2/main.py
@@ -5,8 +5,8 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello Docker"}
+    return {"message": "Hello Docker! This is ElbowPay's FastAPI app running in a Docker container."}
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=5001)
